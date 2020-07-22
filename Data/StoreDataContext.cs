3208c82ec30ff4de76f51bac0d466b02a14a623d
EdwilsonCruz <edwilson_martins@hotmail.com>
2020-07-22 11:51:26 -0300

    adicionando scss ao projeto.

---
+++ Data/StoreDataContext.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using zeusbeta01.Data.Maps;
+using zeusbeta01.Models;
+
+namespace zeusbeta01.Data
+{
+    public class StoreDataContext : DbContext
+    {
+       // public DbSet<Usuario> Usuarios { get; set; }
+       // 
+       // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){
+       //     optionsBuilder.UseSqlServer(@"Server=localhost,1433;Database=slytherinPdtctl;User ID=SA;password=Bianca2018;");
+       // }
+//
+       // protected override void OnModelCreating(ModuleBuilder builder){
+       //     builder.ApplyConfiguration(new UsuarioMap());
+       // }
+    }
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,12 @@
+using System;
+namespace zeusbeta01.Models
+{
+    public class Usuario
+    {
+        
+        public int Id { get; set; }
+        public string Nome {get;set;}
+        public string  Email { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
